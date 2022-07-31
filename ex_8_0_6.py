fd5e90852cb13d7bbebc54845505c2335120ec4d
Sajadahf <sajadahmadian.ee@yahoo.com>
2022-07-31 14:24:40 +0430

    Chapter 8 - Py4e

---
+++ ex_8_0_6.py
@@ -0,0 +1,9 @@
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    words = line.split()
+    print(words[2])
+    emails = words[1]
+    pieces = emails.split('@')
+    print(pieces[1])
