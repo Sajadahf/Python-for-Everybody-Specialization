aa094c69fb4bb0ac6615ce8d5d0f6b6f9cdd3a94
Sajadahf <sajadahmadian.ee@yahoo.com>
2022-07-31 13:56:13 +0430

    Chapter 8 - Py4e

---
+++ ex_8_0_3.py
@@ -0,0 +1,7 @@
+# Built in Functions & Lists
+numbers = [1, 15, 9, 37, 102, 81, 23, 11]
+print('len: ', len(numbers))
+print('max: ', max(numbers))
+print('min: ', min(numbers))
+print('sum: ', sum(numbers))
+print('average: ', sum(numbers) / len(numbers))
