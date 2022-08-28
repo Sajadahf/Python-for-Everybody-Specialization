549f84d2c7695f58e73e2c964eb97ab4713058f7
Sajadahf <sajadahmadian.ee@yahoo.com>
2022-08-28 22:00:22 +0430

    Py4e - Chapter 11

---
+++ ex_11_3_1.py
@@ -0,0 +1,15 @@
+import re
+
+hand = open("regex_sum_1607250.txt")
+
+sum = 0
+i = 0
+numlist = list()
+for line in hand:
+    line = line.rstrip()
+    stuff = re.findall("[0-9]+", line)
+    for num in stuff:
+        i += 1
+        sum = sum + int(num)
+
+print("There are", i, "values with a sum=", sum)
