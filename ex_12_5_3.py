d5c0d3d9b7ce645f1d06fea196cab84928a8daa2
Sajadahf <sajadahmadian.ee@yahoo.com>
2022-09-09 02:24:59 +0430

    Py4e - Chapter 12

---
+++ ex_12_5_3.py
@@ -0,0 +1,17 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+countos = 0
+sum = 0
+
+spans = soup('span')
+for span in spans:
+    sum += int(span.contents[0])
+    countos += 1
+
+print('Count ', countos)
+print('Sum ', sum)
