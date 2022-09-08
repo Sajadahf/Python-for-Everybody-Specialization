4c7ce081d509e6bba406c0340bff5e09275d923f
Sajadahf <sajadahmadian.ee@yahoo.com>
2022-09-09 02:25:07 +0430

    Py4e - Chapter 12

---
+++ ex_12_5_4.py
@@ -0,0 +1,27 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+current_repeat_count = 0
+url = input("Enter - ")
+repeat_count = int(input("Enter count: "))
+position = int(input("Enter position: "))
+
+
+def parsehtml(url):
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+    tags = soup('a')
+    return tags
+
+while current_repeat_count < repeat_count:
+    print("Retrieving: ", url)
+    tags = parsehtml(url)
+    for index, item in enumerate(tags):
+        if index == position - 1:
+            url = item.get('href', None)
+            name = item.contents[0]
+            break
+        else:
+            continue
+    current_repeat_count += 1
+print("Last_Url: ", url)
